d65856862bf4defe3f4630c1c750af347dc0e642
victorokonkwo67@gmail.com <victorokonkwo67@gmail.com>
2024-11-01 09:33:17 +0100

    chore: implement palindrome function

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let res = is_palindrome("civic");
+    println!("{res}")
+}
+
+fn is_palindrome(word: &str) -> bool {
+    let mut start_index = 0;
+    let mut end_index = word.len() - 1;
+
+    while start_index < end_index {
+        if word.as_bytes()[start_index] != word.as_bytes()[end_index] {
+            return false;
+        }
+
+        start_index += 1;
+        end_index -= 1;
+    }
+
+    return true;
+}
